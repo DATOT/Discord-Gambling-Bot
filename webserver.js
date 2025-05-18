c3e6ad9eba70bef478b2b19fb3fd5f9edb12e1c8
Tabby <datotodev@gmail.com>
2025-05-18 13:38:32 +0700

    Discord-Gambling-Bot-v1.0.2

--- webserver.js
+++ webserver.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
 
+console.log('In webserver right now:3');
+console.log(`Port: ${port}`);
+
 app.get('/', (req, res) => {
 	res.send('Gambling Bot is alive nya~ :3');
+	console.log('Gambling Bot is alive nya~ :3');
 });
 
 app.listen(port, () => {
